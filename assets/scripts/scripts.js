ef27f892ffb3af8d506fea3a3d5ca707fd867829
Modestas Bujanauskas <mbujanauskas@gmail.com>
2018-08-21 18:50:48 +0300

    Correct

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,54 @@
+//On scroll add class sticky background to element with ID #header
+/* BUGS:
+    --sticky-bakground uzlenda uz nuotraukos self-img
+*/
+const navbar = document.querySelector('#header');
+const sticky = navbar.offsetHeight;
+
+const addSticky = () => {
+    // console.log(sticky)
+    // console.log(window.pageYOffset)
+    if (window.pageYOffset >= sticky) {
+        
+        navbar.classList.add("sticky-background")
+    } else {
+        navbar.classList.remove("sticky-background");
+    } 
+}
+
+window.onscroll = addSticky;
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+      slideIndex = 1
+  }    
+  if (n < 1) {
+      slideIndex = slides.length
+  }
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";     
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
