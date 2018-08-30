4d90a2ec4b8fd3b686b8bfb03724023c48277637
Modestas Bujanauskas <mbujanauskas@gmail.com>
2018-08-30 15:36:21 +0300

    Add Smooth scroll js code

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -84,15 +84,63 @@ function progressBarLoader(){
 
 progressBarLoader()
 
-// function hashJumpFix(){
-//     elements = document.querySelectorAll("nav a");
-//     elements.forEach((element) => {
-//         element.addEventListener("click", () =>{
-//             if (!(element.innerHTML === "Contact")) {
-//                 setTimeout(() => window.scrollBy(0, -110), 10);
-//             }
-//         });
-//     });
-// };
+function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
 
-// hashJumpFix();
+function elmYPosition(eID) {
+    var elm = document.querySelector(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;   
+    } 
+    return y; 
+}
+
+function smoothScroll(eID) {
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    if(eID !== '#contact') {
+        stopY = stopY - 110;
+    }
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+        scrollTo(0, stopY); return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+}
+
+function addSmoothScroll(){
+    elements = document.querySelectorAll("nav a");
+    elements.forEach((element) => {
+        element.addEventListener("click", () =>{
+            eID = element.getAttribute('href');
+            smoothScroll(eID); 
+        });
+    });
+};
+
+addSmoothScroll();
